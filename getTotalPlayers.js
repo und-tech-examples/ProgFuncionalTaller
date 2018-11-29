fd90903358aa16bc98ebcb201f3f266d4fc969c8
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2018-11-29 09:43:15 -0500

    feat(funciones): se añade funciones de la seleccion peruana de futbol

---
+++ getTotalPlayers.js
@@ -0,0 +1,13 @@
+*
+*  @Description: Obtiene el total de jugadores de futbol
+*  
+*  @Language: Javascript
+*/
+
+// @function
+function getTotalPlayers(){
+  return 11;
+}
+
+// @execute
+getTotalPlayers();
